740aa2d941d9793e71ae10c072948f0f52da869a
Alejandro Sanchez Fernandez <alexius1996@gmail.com>
2023-11-22 09:56:58 +0100

    Login 90% hecho y añadidos TODO de registro

--- controladores/UsuarioController.php
+++ controladores/UsuarioController.php
@@ -6,22 +6,32 @@ require_once "librerias/Token.php";
 
 class UsuarioController extends Controller{
 
-
+    //Genero un token nuevo, guardo en session y rederizo la vista pasando el token
     public function renderLogin(){
-
-        //Genero un token nuevo, guardo en session y pido la vista
         $token = Token::tokenizer()->getToken();
         $_SESSION["token"] = $token;
-
         $this->render("usuario/login.php.twig", ["token" => $token]);
     }
 
+    //Comprueba el token y si existe el usuario en la base de datos
     public function login(){
-        if ($_POST["_csrf"] != $_SESSION["token"]){
+        if ($_POST["_csrf"] != $_SESSION["token"] || empty($_POST["email"]) || empty($_POST["pass"])){
             redireccion("main");
+        }else{
+            $usuario = Usuario::loginUsuario($_POST["email"], $_POST["pass"]);
+            if (is_null($usuario)) {
+                redireccion("main");
+                //TODO add error correo o contraseña no correcto
+                $_SESSION['errorMensaje'] = "Contraseña o correo no correcto.";
+            }
+            $_SESSION["inicio"]  = time();
+            echo "Usuario logueado";
         }
     }
 
+    public function registrar(){
+        //TODO
+    }
 
 
 }
--- modelos/Usuario.php
+++ modelos/Usuario.php
@@ -19,7 +19,21 @@ class Usuario {
         //TODO
     }
 
+    //Consulta a la DB y hasheo de pw
     public static function loginUsuario(string $email, string $pw){
+        //TODO
+        $pdo = Conexion::getConnection();
+        $stmt = $pdo->prepare("SELECT * FROM Usuarios WHERE email = :correo AND contraseña = :pw");
+        $stmt->bindValue(':correo', $email, PDO::PARAM_STR);
+        $pw = password_hash($pw, PASSWORD_ARGON2ID);
+        $stmt->bindValue(':pw', $pw, PDO::PARAM_STR);
+        $stmt->execute();
+
+        //TODO FINALIZAR
+        unset($pdo);
+    }
+
+    public static function registrarUsuario(){
         //TODO
     }
 
