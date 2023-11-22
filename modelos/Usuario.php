927522af817d8bf08beb89cbfa57a0d7d9029fd3
Alejandro Sanchez Fernandez <alexius1996@gmail.com>
2023-11-22 10:30:28 +0100

    gestionado resultado del login

--- modelos/Usuario.php
+++ modelos/Usuario.php
@@ -22,15 +22,30 @@ class Usuario {
     //Consulta a la DB y hasheo de pw
     public static function loginUsuario(string $email, string $pw){
         //TODO
-        $pdo = Conexion::getConnection();
+        $pdo = Conexion::getConnection()->getPdo();
         $stmt = $pdo->prepare("SELECT * FROM Usuarios WHERE email = :correo AND contraseña = :pw");
         $stmt->bindValue(':correo', $email, PDO::PARAM_STR);
         $pw = password_hash($pw, PASSWORD_ARGON2ID);
         $stmt->bindValue(':pw', $pw, PDO::PARAM_STR);
         $stmt->execute();
+        $resultado = $stmt->fetch(PDO::FETCH_ASSOC);
 
-        //TODO FINALIZAR
-        unset($pdo);
+        if ($resultado) {
+            $usuario = new Usuario();
+            $usuario->idUsuario = $resultado['idUsuario'];
+            $usuario->email = $resultado['email'];
+            $usuario->DNI = $resultado['DNI'];
+            $usuario->pw = $resultado['pw'];
+            $usuario->nombre = $resultado['nombre'];
+            $usuario->apellido = $resultado['apellido'];
+            $usuario->telefono = $resultado['telefono'];
+            unset($pdo);
+            return $usuario;
+        } else {
+            unset($pdo);
+            return null;
+        }
+        
     }
 
     public static function registrarUsuario(){
