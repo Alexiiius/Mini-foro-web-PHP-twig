db6e8b1f74fefbf4ca5bb0804cfe9fa520eff99e
Alejandro Sanchez Fernandez <alexius1996@gmail.com>
2023-11-27 13:08:00 +0100

    comentado la clase

--- librerias/twigExtension.php
+++ librerias/twigExtension.php
@@ -3,8 +3,9 @@
     // importamos la librería TWIG
     require_once "vendor/autoload.php" ;
 
-    class TwigExtensions extends \Twig\Extension\AbstractExtension
-                         implements \Twig\Extension\GlobalsInterface {
+    //Clase que extiende de la clase AbstractExtension de Twig para poder usarla en las vistas y pasarle variables globales como la sesion
+    //Accesible desde la vista con _session.variable
+    class TwigExtensions extends \Twig\Extension\AbstractExtension implements \Twig\Extension\GlobalsInterface {
 
 
         public function getGlobals(): array {
