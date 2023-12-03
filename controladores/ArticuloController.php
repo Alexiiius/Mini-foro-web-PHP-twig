43b8f88184ed2b3b98436b59f7be2a0e0bd8a69c
Alejandro Sanchez Fernandez <alexius1996@gmail.com>
2023-12-03 21:19:00 +0100

    creada funcion apra puntuar articulos y control de mensajes via get al renderizar twig

--- controladores/ArticuloController.php
+++ controladores/ArticuloController.php
@@ -7,6 +7,7 @@ require_once "librerias/Token.php";
 
 class ArticuloController extends Controller{
 
+    const MENSAJE = [null, "Puntuaje enviado correctamente.", "Error al puntuar."];
 
     //Renderizo la vista de registro cargando todos los registros de la DB
     //Se actualiza la sesion  agregando 5 minutos mas y se comprueba si existe el usuario en la sesion
@@ -15,10 +16,34 @@ class ArticuloController extends Controller{
 
         $this->sessionUpdate();
         if (isset($_SESSION["usuario"])) {
+            $idMensaje = $_GET["mensaje"]??0;
+
+            if ($idMensaje < 0 || $idMensaje > 2) {
+                $idMensaje = 0;
+            }
+
+            $token = Token::tokenizer()->getToken();
+            $_SESSION["token"] = $token;
             $articulos = Articulo::getArticulos();
-            $this->render("articulo/home.php.twig", ["articulos" => $articulos]);
+            $this->render("articulo/home.php.twig", ["articulos" => $articulos, "token" => $token, "mensaje" => self::MENSAJE[$idMensaje]]);
         }else{  
             $this->redireccion("login");
+        }
+    }
+
+    //Se añade el rating del articulo a la DB
+    public function puntuarArticulo(){
+
+        $this->sessionUpdate();
+        if ($_POST["_csrf"] != $_SESSION["token"] || empty($_POST["nota"]) || empty($_POST["id_articulo"]) || empty($_POST["id_usuario"])){
+            $this->redireccion("login");
+        }else{
+            $resultado = Articulo::puntuar($_POST["id_articulo"], $_POST["id_usuario"], $_POST["nota"]);
+            if ($resultado) {
+                $this->redireccion("home?mensaje=1");
+            }else{
+                $this->redireccion("home?mensaje=2");
+            }
         }
     }
 
