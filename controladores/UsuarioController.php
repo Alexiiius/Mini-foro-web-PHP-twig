fefc2a0f94439a05a1d79961d12d846a4875b70a
Alejandro Sanchez Fernandez <alexius1996@gmail.com>
2023-12-03 20:55:41 +0100

    Añadido cierre de sesion y gestiones

--- controladores/UsuarioController.php
+++ controladores/UsuarioController.php
@@ -10,7 +10,13 @@ class UsuarioController extends Controller{
 
     //Genero un token nuevo, guardo en session y rederizo la vista pasando el token
     //Mediante GET se esta pasando el numero del mensaje que contiene el array MENSAJE para mostrar el mensaje correspondiente
+    //Se comprueba si hay una session ya iniciada y se redirige a la pagina de inicio.
     public function renderLogin(){
+
+        if (isset($_SESSION["usuario"])) {
+            $this->redireccion("home");
+        }
+
         $token = Token::tokenizer()->getToken();
         $_SESSION["token"] = $token;
         $idMensaje = $_GET["mensaje"]??0;
@@ -31,21 +37,33 @@ class UsuarioController extends Controller{
             if (is_null($usuario)) {
                 $this->redireccion("login?mensaje=1");
                 //$this->renderLogin("Contraseña o correo no correcto.");
+            }else{
+                $_SESSION["inicio"]  = time();
+                $_SESSION["expira"]  = $_SESSION["inicio"] + (5 * 60);  //5 minutos para expirar sesion
+                $_SESSION["usuario"] = $usuario; //Guardo el objeto usuario en la sesion
+                $this->redireccion("home");
             }
-            $_SESSION["inicio"]  = time();
-            $_SESSION["expira"]  = $_SESSION["inicio"] + (5 * 60);  //5 minutos para expirar sesion
-            $_SESSION["usuario"] = serialize($usuario); //Guardo el objeto usuario en la sesion serializado
-            $this->redireccion("home");
         }
     }
 
     //Renderizo la vista de registro
     //En caso de que el mensaje no este entre 0 y 3 se pone el mensaje por defecto
     public function renderRegistro(){
+
+        if (isset($_SESSION["usuario"])) {
+            $this->redireccion("home");
+        }
+
         $idMensaje = $_GET["mensaje"]??0;
+
+        if ($idMensaje !== 3 && $idMensaje !== 0) {
+            $idMensaje = 0;
+        }
+        
         $this->render("usuario/registro.php.twig", ["mensaje" => self::MENSAJE[$idMensaje] ]);
     }
 
+    //Comprueba el token y si los campos estan vacios. Despues registra al usuario en la base de datos o redirige al registro con un mensaje de error
     public function registro(){
         if ( empty($_POST["email"]) || empty($_POST["nombre"]) || empty($_POST["pass"])){
             $this->redireccion("registro");
@@ -59,6 +77,14 @@ class UsuarioController extends Controller{
             //$this->renderRegistro("Error al registrar usuario.");
             $this->redireccion("registro?mensaje=3");
         }
+    }
+
+
+    //Cerrar sesion y redirigir al login
+    public function cerrarSesion(){
+        session_unset();
+        session_destroy();
+        $this->redireccion("login");
     }
 
 
