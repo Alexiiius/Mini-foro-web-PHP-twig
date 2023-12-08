6e60cbc5c195b0e5ad2f3af3941394c88f340d1b
Alejandro Sanchez Fernandez <alexius1996@gmail.com>
2023-12-08 23:01:13 +0100

    Se mejoran los metodos y se crean nuevos para gestionar el listado, borrado y puntuaje de articulos de otros usuarios y no permitir puntuar los tuyos mismos.

--- controladores/ArticuloController.php
+++ controladores/ArticuloController.php
@@ -7,18 +7,17 @@ require_once "librerias/Token.php";
 
 class ArticuloController extends Controller{
 
-    const MENSAJE = [null, "Puntuaje enviado correctamente.", "Error al puntuar."];
+    const MENSAJE = [null, "Puntuaje enviado correctamente.", "Error al puntuar.", "Error al crear articulo.", "Articulo creado correctamente.", "Articulo eliminado correctamente.", "Error al eliminar articulo."];
 
     //Renderizo la vista de registro cargando todos los registros de la DB
     //Se actualiza la sesion  agregando 5 minutos mas y se comprueba si existe el usuario en la sesion
     //De no existir se redirige a la pagina de login
     public function renderHome(){
-
         $this->sessionUpdate();
         if (isset($_SESSION["usuario"])) {
             $idMensaje = $_GET["mensaje"]??0;
 
-            if ($idMensaje < 0 || $idMensaje > 2) {
+            if ($idMensaje < 0 || $idMensaje > 6) {
                 $idMensaje = 0;
             }
 
@@ -31,18 +30,83 @@ class ArticuloController extends Controller{
         }
     }
 
-    //Se añade el rating del articulo a la DB
-    public function puntuarArticulo(){
-
+    //Renderiza la vista de los articulos del usuario logueado de la pagina "Mis articulos"
+    public function renderPersonal(){
         $this->sessionUpdate();
-        if ($_POST["_csrf"] != $_SESSION["token"] || empty($_POST["nota"]) || empty($_POST["id_articulo"]) || empty($_POST["id_usuario"])){
+        if (isset($_SESSION["usuario"])) {
+            $idMensaje = $_GET["mensaje"]??0;
+
+            if ($idMensaje < 0 || $idMensaje > 4) {
+                $idMensaje = 0;
+            }
+
+            $token = Token::tokenizer()->getToken();
+            $_SESSION["token"] = $token;
+
+            $id = $_GET['id'] ?? null;
+
+            if ($id === null) {
+                $articulos = Articulo::getArticulosAutor($_SESSION["usuario"]->idUsuario);
+            }else{
+                $articulos = Articulo::getArticulosAutor($id);
+            }
+
+            
+            $this->render("articulo/personal.php.twig", ["articulos" => $articulos, "token" => $token, "mensaje" => self::MENSAJE[$idMensaje]]);
+        }else{  
+            $this->redireccion("login");
+        }
+    }
+    
+    //Se crea un articulo en la DB
+    public function crearArticulo(){
+        $this->sessionUpdate();
+        if ($_POST["_csrf"] != $_SESSION["token"] || empty($_POST["titulo"]) || empty($_POST["contenido"]) || empty($_SESSION["usuario"]->idUsuario)){
             $this->redireccion("login");
         }else{
-            $resultado = Articulo::puntuar($_POST["id_articulo"], $_POST["id_usuario"], $_POST["nota"]);
+            $resultado = Articulo::crear($_POST["titulo"], $_POST["contenido"], $_SESSION["usuario"]->idUsuario);
             if ($resultado) {
-                $this->redireccion("home?mensaje=1");
+                $this->redireccion("home?mensaje=4");
             }else{
-                $this->redireccion("home?mensaje=2");
+                $this->redireccion("home?mensaje=3");
+            }
+        }
+    }
+
+
+    //Se añade el rating del articulo a la DB
+    //Si el usuario ya puntuo el mismo articulo, se hace un update en vez de un insert.
+    public function puntuarArticulo(){
+        $this->sessionUpdate();
+
+        if ($_POST["_csrf"] != $_SESSION["token"] || empty($_POST["nota"]) || empty($_POST["id_articulo"]) || empty($_POST["id_usuario"])){
+            $this->redireccion("login");
+            return;
+        }
+ 
+        if (Articulo::yaPuntuo($_POST["id_articulo"], $_POST["id_usuario"])) {
+            $resultado = Articulo::actualizarPuntuaje($_POST["id_articulo"], $_POST["id_usuario"], $_POST["nota"]);
+        } else {
+            $resultado = Articulo::puntuar($_POST["id_articulo"], $_POST["id_usuario"], $_POST["nota"]);
+        }
+    
+        if ($resultado) {
+            $this->redireccion("home?mensaje=1");
+        } else {
+            $this->redireccion("home?mensaje=2");
+        }
+    }
+
+    public function eleminarArticulo(){
+        $this->sessionUpdate();
+        if ($_POST["_csrf"] != $_SESSION["token"] || empty($_POST["id_articulo"]) || empty($_SESSION["usuario"]->idUsuario)){
+            $this->redireccion("login");
+        }else{
+            $resultado = Articulo::eliminar($_POST["id_articulo"], $_SESSION["usuario"]->idUsuario);
+            if ($resultado) {
+                $this->redireccion("home?mensaje=5");
+            }else{
+                $this->redireccion("home?mensaje=6");
             }
         }
     }
--- modelos/Articulo.php
+++ modelos/Articulo.php
@@ -11,9 +11,11 @@ class Articulo {
     public string $fecha;
 
 
+    //No se esta usando actualmente ni el constructor ni sus atributos, no se isntancia ningun objeto de esta clase
     public function __construct(){}
 
 
+    //no se esta usndo actualmente, obtiene un articulo segun su id
     public static function getArticulo(int $i){
         $pdo = Conexion::getConnection()->getPdo();
         $stmt = $pdo->prepare("SELECT * FROM Articulos WHERE idArticulo = :idArticulo");
@@ -40,16 +42,24 @@ class Articulo {
                                 FROM Articulos 
                                 JOIN Usuarios ON Articulos.id_autor = Usuarios.idUsuario 
                                 LEFT JOIN Ratings ON Articulos.idArticulo = Ratings.id_articulo 
-                                GROUP BY Articulos.idArticulo");
+                                GROUP BY Articulos.idArticulo
+                                ORDER BY fecha_publicacion DESC");
         $stmt->execute();
         $resultado = $stmt->fetchAll(PDO::FETCH_ASSOC);
         unset($pdo);
         return $resultado;
     }
 
+    //Devuelve un array con todos los articulos pertenecientes a un autor en concreto
     public static function getArticulosAutor(int $idAutor){
         $pdo = Conexion::getConnection()->getPdo();
-        $stmt = $pdo->prepare("SELECT * FROM Articulos WHERE idAutor = :idAutor");
+        $stmt = $pdo->prepare("SELECT Articulos.*, Usuarios.nombre, ROUND(AVG(Ratings.puntuaje), 1) as rating 
+                                FROM Articulos 
+                                JOIN Usuarios ON Articulos.id_autor = Usuarios.idUsuario 
+                                LEFT JOIN Ratings ON Articulos.idArticulo = Ratings.id_articulo 
+                                WHERE id_autor = :idAutor
+                                GROUP BY Articulos.idArticulo
+                                ORDER BY fecha_publicacion DESC;");
         $stmt->bindValue(':idAutor', $idAutor, PDO::PARAM_INT);
         $stmt->execute();
         $resultado = $stmt->fetchAll(PDO::FETCH_ASSOC);
@@ -57,36 +67,94 @@ class Articulo {
         return $resultado;
     }
 
-    public static function borrarArticulo(int $idArticulo){
+    //Elimina un articulo concreto perteneciente a un usuario concreto
+    public static function eliminar(int $idArticulo, int $idUsuario){
         $pdo = Conexion::getConnection()->getPdo();
-        $stmt = $pdo->prepare("DELETE FROM Articulos WHERE idArticulo = :idArticulo");
+    
+        //primero elimina los ratings asociados al articulo
+        $stmt = $pdo->prepare("DELETE FROM Ratings WHERE id_articulo = :idArticulo");
         $stmt->bindValue(':idArticulo', $idArticulo, PDO::PARAM_INT);
-        $stmt->execute();
-        $resultado = $stmt->rowCount();
+        try {
+            $stmt->execute();
+        } catch (PDOException $e) {
+            unset($pdo);
+            return $resultado = 0;
+        }
+    
+        //luego elimina el articulo
+        $stmt = $pdo->prepare("DELETE FROM Articulos WHERE idArticulo = :idArticulo AND id_autor = :idUsuario");
+        $stmt->bindValue(':idArticulo', $idArticulo, PDO::PARAM_INT);
+        $stmt->bindValue(':idUsuario', $idUsuario, PDO::PARAM_INT);
+        try {
+            $stmt->execute();
+            $resultado = $stmt->rowCount();
+        } catch (PDOException $e) {
+            $resultado = 0;
+        }
+    
         unset($pdo);
         return $resultado;
     }
-
-    public static function registrarArticulo(int $idAutor, string $titulo, string $contenido){
-        $pdo = Conexion::getConnection()->getPdo();
-        $stmt = $pdo->prepare("INSERT INTO Articulos (idAutor, titulo, contenido) VALUES (?, ?, ?)");
-        $stmt->bindParam(1, $idAutor, PDO::PARAM_INT);
-        $stmt->bindParam(2, $titulo, PDO::PARAM_STR);
-        $stmt->bindParam(3, $contenido, PDO::PARAM_STR);
-        $stmt->execute();
-        $resultado = $stmt->rowCount();
-        unset($pdo);
-        return $resultado;
-    }
-
 
     //Registra el rating de un articulo en la DB junto con el usuario que lo puntuo
-    public static function puntuar(int $idArticulo, int $idUsuario, int $puntuaje){
+    public static function puntuar(int $idArticulo, int $idUsuario, string $puntuaje){
         $pdo = Conexion::getConnection()->getPdo();
         $stmt = $pdo->prepare("INSERT INTO Ratings (id_articulo, id_usuario, puntuaje) VALUES (?, ?, ?)");
-        $stmt->bindParam(1, $idArticulo, PDO::PARAM_INT);
-        $stmt->bindParam(2, $idUsuario, PDO::PARAM_INT);
-        $stmt->bindParam(3, $puntuaje, PDO::PARAM_INT);
+        $stmt->bindValue(1, $idArticulo, PDO::PARAM_INT);
+        $stmt->bindValue(2, $idUsuario, PDO::PARAM_INT);
+        $puntuajeFloat = floatval($puntuaje);
+        $stmt->bindValue(3, $puntuajeFloat);
+        try {
+            $stmt->execute();
+            $resultado = $stmt->rowCount();
+        } catch (PDOException $e) {
+            $resultado = 0;
+        }
+        unset($pdo);
+        return $resultado;
+    }
+
+    //Crea un articulo en la DB
+    public static function crear(string $titulo, string $contenido, int $idUsuario){
+        $pdo = Conexion::getConnection()->getPdo();
+        $stmt = $pdo->prepare("INSERT INTO Articulos (titulo, contenido, id_autor) VALUES (?, ?, ?)");
+        $stmt->bindParam(1, $titulo, PDO::PARAM_STR);
+        $stmt->bindParam(2, $contenido, PDO::PARAM_STR);
+        $stmt->bindParam(3, $idUsuario, PDO::PARAM_INT);
+        try {
+            $stmt->execute();
+            $resultado = $stmt->rowCount();
+        } catch (PDOException $e) {
+            $resultado = 0;
+        }
+        unset($pdo);
+        return $resultado;
+    }
+
+    //Devuelve el rating de un articulo en la DB junto con el usuario que lo puntuo
+    public static function yaPuntuo(int $idArticulo, int $idUsuario){
+        $pdo = Conexion::getConnection()->getPdo();
+        $stmt = $pdo->prepare("SELECT * FROM Ratings WHERE id_articulo = :idArticulo AND id_usuario = :idUsuario");
+        $stmt->bindValue(':idArticulo', $idArticulo, PDO::PARAM_INT);
+        $stmt->bindValue(':idUsuario', $idUsuario, PDO::PARAM_INT);
+        try {
+            $stmt->execute();
+            $resultado = $stmt->rowCount();
+        } catch (PDOException $e) {
+            $resultado = 0;
+        }
+        unset($pdo);
+        return $resultado;
+    }
+
+    //Actualiza el rating de un articulo en la DB junto con el usuario que lo puntuo
+    public static function actualizarPuntuaje(int $idArticulo, int $idUsuario, string $puntuaje){
+        $pdo = Conexion::getConnection()->getPdo();
+        $stmt = $pdo->prepare("UPDATE Ratings SET puntuaje = :puntuaje WHERE id_articulo = :idArticulo AND id_usuario = :idUsuario");
+        $stmt->bindValue(':idArticulo', $idArticulo, PDO::PARAM_INT);
+        $stmt->bindValue(':idUsuario', $idUsuario, PDO::PARAM_INT);
+        $puntuajeFloat = floatval($puntuaje);
+        $stmt->bindValue(':puntuaje', $puntuajeFloat);
         try {
             $stmt->execute();
             $resultado = $stmt->rowCount();
